39f963b4c6e65652ff56a24ab4e33db3d587a233
EleetCodeR <>
2019-12-28 21:40:46 +0530

    Modified instructions

--- CodeKitchen/Python Scripts/4_Classes/10_Properties.py
+++ CodeKitchen/Python Scripts/4_Classes/10_Properties.py
@@ -1,4 +1,4 @@
-# Sometimes we need more control over atteributes.
+# Sometimes we need more control over attributes.
 
 
 class Product:
@@ -9,14 +9,14 @@ class Product:
     # def get_price(self):
     #     return self.__price
 
-    @property
-    def price(self):
-        return self.price
-
     # def set_price(self, value):
     #     if value < 0:
     #         raise ValueError("Price can not be negative.")
     #     self.__price = value
+
+    @property
+    def price(self):
+        return self.price
 
     @price.setter
     def price(self, value):
@@ -24,20 +24,32 @@ class Product:
             raise ValueError("Price can not be negative.")
         self.__price = value
 
-        # price = property(get_price, set_price)
-        # Property is an object that can be used to get/set the value of an attribute.(i.e to manage an attribute.)
 
+# product = Product(-50)
 
-# Raising error like above is not "Pythonic"(doesn't use full potential of python.)
-#product = Product(-50)
+# # Raising error like above (in set_price), and using getter/setter like above is not "Pythonic"(doesn't use full potential of python.)
+# # Lets define a property instead as shown below:
+
+# price = property(get_price, set_price) # see NOTE:1
+
+# product = Product(10)
+# print(product.price)
+# product.price = 2
+# print(product.price)
+
+# NOTE: 1
+# Property is an object of class Property ,that can be used to get/set the value of an attribute.(here 'price').
+# Here we have also provided references as parameters to the methods that should be used for get/set of price,
+# which are called internally.
 
 
 product = Product(10)
 product.price = 2
 print(product.price)
 
-# also we can access geter/seter methods from object and at types we might want to keep our interface clear and concise.
-# so instead of calling property function we can use decorators, and we change geter/seter method names.
-# for above get'er method @property and above set'ter method @<name of attribute>.setter
-# If we do not write set'er method , then it becomes a read only property.
-# (in  trying to modify value will throw - AttributeError: can't set attribute)
+# NOTE : 2
+# Even after defining the property, we can still see getter/setter methods (get_price, set_price) in the object interface.(also in IDE intellisense)
+# so to keep our object interface clean and simple we can use anotations (@property and @price.setter),
+# Instead of calling property function.
+# i.e, Above getter method write @property and above setter method write @<name of attribute>.setter (here @price.setter)
+# If we do not write setter method , then it becomes a read only property.(in trying to modify value will throw - AttributeError: can't set attribute)
--- CodeKitchen/Python Scripts/4_Classes/9_PrivateMembers.py
+++ CodeKitchen/Python Scripts/4_Classes/9_PrivateMembers.py
@@ -1,5 +1,6 @@
 class TagCloud:
     def __init__(self):
+        #self.tags = {}
         self.__tags = {}
 
     def add(self, tag):
@@ -26,14 +27,16 @@ cloud.add("PYTHON")
 
 print(cloud["PYTHON"])   # o/p :- 4
 
-# print(cloud.__tags["PYTHON"])  # KeyError
-# This error occured because we could directly access attribute "tags".
-# To make this attribute private, use prefix __ (double underscore.) for attri."tag"
+# print(cloud.tags["PYTHON"])  # KeyError
 
-# NOTE : So to make attribute/method as private prefix them with __ (double underscore.)
+# This error occured because we could directly access attribute "tags".
+# To make this attribute private (by convention), use prefix __ (double underscore.) for attri."tag"
+
+# NOTE : So to make attribute/even a method, as private prefix them with __ (double underscore.)
 # NOTE : however in python, making members private by this prefix is not a security feature, but a convention(to prevent accidental access);
 # Because we can still access private members using __dict__ method, which is defined by default for every class/object;
 # It consists of all the attributes of that particular class;
 # The attributes in this dict. are automatically prefixed with classname by python interpreter.
 print(cloud.__dict__)  # o/p :-  {'_TagCloud__tags': {'python': 4}}
-print(cloud._TagCloud__tags)  # returns our dictionary 'tags'
+print(cloud._TagCloud__tags)
+# basically returns our (private member) dictionary 'tags'.
