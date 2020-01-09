64589f663ee38de1d0217d93e87932593dafc52c
EleetCodeR <>
2020-01-09 11:13:37 +0530

    Random Value Generation, Shuffling values
    Generating Random values,
    using to generate random passwords,
    Array shuffling.

---
+++ CodeKitchen/Python Scripts/6_PythonStdLib/11_GeneratingRandomValues.py
@@ -0,0 +1,33 @@
+import random
+import string
+
+print(random.random()) # gives any random floating point number.
+print(random.randint(1,10)) # enumerates between given range of integer numbers.
+print(random.choice([2,4,6,8,10])) # Takes an array/sequence of elements and randomly picks an element from array.
+print(random.choice(["An","Arrray","of","Strings."])) # Takes an array/sequence of elements and randomly picks an element from array.
+
+print(random.choice("abcdefghijklmnopqrstuvwxyz0123456789")) # picks one element ; randomly from sequence
+# to pick more than one element at random;
+print(random.choices("abcdefghijklmnopqrstuvwxyz0123456789",k=4)) 
+ # but this will return a list of size k,
+
+# to join these element by a delimiter we can use join method from class string.
+print("".join(random.choices("abcdefghijklmnopqrstuvwxyz0123456789",k=4)))
+# the string on which join is called is inserted between the iterable-string-object and result is given as a string.
+# this can be used to generated random passwords from a given sequence.
+
+print(",".join(random.choices("abcdefghijklmnopqrstuvwxyz0123456789",k=4)))
+
+# instead of typing all the characters like in above. we can use string class.
+print(string.ascii_letters) # all lowercase and uppercase letters.
+print(string.ascii_lowercase)
+print(string.ascii_uppercase)
+print(string.digits)
+
+# so a better way for generating random passwords is,
+print("".join(random.choices(string.ascii_letters+string.digits,k=4)))
+
+# RANDOM SHUFFLING AN ARRAY :
+numbers = [1,2,3,4,5,6]
+random.shuffle(numbers)
+print(numbers)
