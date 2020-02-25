8f78b076224add317e9f38c7f65db034191d32b7
Vishal Ramane <vramane007@gmail.com>
2020-02-25 09:07:00 +0530

    py practice scripts

---
+++ CodeKitchen/Python Scripts/6_PythonStdLib/12_OpeningThe Browser.py
@@ -0,0 +1,5 @@
+# Assuming a scenario where we want to run a script to deploy code and then open the web-browser with target URL.
+import webbrowser
+
+print("Deployment is Completed.")
+webbrowser.open("http://google.com")
---
+++ CodeKitchen/Python Scripts/6_PythonStdLib/13_.sendingEmail.py
@@ -0,0 +1,5 @@
+from email.mime.multipart import MIMEMultipart
+
+message = MIMEMultipart()
+message["from"] = "Vishal Ramane"
+message["to"] = "testuser@"
