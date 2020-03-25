9ea1dd854bcf7aad7e6bf8a79fa56530a28b2b24
Vishal Ramane <vramane007@gmail.com>
2020-03-25 19:20:16 +0530

    Node practice

--- CodeKitchen/NodeJS/ModuleSys/2_logger.js
+++ CodeKitchen/NodeJS/ModuleSys/2_logger.js
@@ -1,10 +1,20 @@
-
+// ====== after 6_eventModule
+// lets raise and handle events,
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+// after 6_eventModule ====== 
 
 var url = 'http://mylogger.io/log';
 
 function log(message) {
     // code to send HTTP request.
     console.log(message);
+
+    // ====== after 6_eventModule
+    // raise event,
+    message = 'Event:logged a message!!';
+    emitter.emit("logging", message);
+    // after 6_eventModule ====== 
 };
 
 // ====== to be able to access this function outside of this module,
@@ -20,3 +30,8 @@ module.exports = log;
 
 console.log(__filename);
 console.log(__dirname);
+
+
+
+
+
