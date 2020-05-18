cf8d4f3c0559c5acef22361619583ba2b10b4f18
Vishal Ramane <vramane007@gmail.com>
2020-05-19 00:20:13 +0530

    learning nodejs

--- CodeKitchen/NodeJS/ModuleSys/1_GlobalObj.js
+++ CodeKitchen/NodeJS/ModuleSys/1_GlobalObj.js
@@ -1,7 +1,7 @@
 // console.log(); // global
 // console object is a type of global object. (Global Scope.)
 
-// similarly following are properties of global object ,
+// similarly following are methods of global object ,
 // setTimeout()
 // clearTimeout()
 // setInterval()
@@ -12,12 +12,11 @@
 //global.console.log()
 // but variables or functions (user defined) such as below can not be accessed using this.
 // not added into global scope, (they have file/module scope)
-var message = '';
+var message = "";
 console.log(global.message);
 
-// in browser we have window object that represents global scope.
+// in browser we have window object that represents a browser window,has global scope.
 //window.console.log()
 //window.setTimeout()
 // var message =''
 // above variables is also accesible via window object inside the browser.
-
--- CodeKitchen/NodeJS/expressDemo/httpGet.js
+++ CodeKitchen/NodeJS/expressDemo/httpGet.js
@@ -1,34 +1,29 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 
 const courses = [
-    { id: 1, name: 'course1' },
-    { id: 2, name: 'course2' },
-    { id: 3, name: 'course3' }
-]
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
 // Route-1
-app.get('/', (req, res) => {
-    res.send('Hello World!!');
+app.get("/", (req, res) => {
+  res.send("Hello World!!");
 });
 
 // Route-2
-app.get('/api/courses', (req, res) => {
-    // send any object or access database.
-    res.send(courses);
+app.get("/api/courses", (req, res) => {
+  // send any object or access database.
+  res.send(courses);
 });
 
 // Route-3 with parameters
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course) res.status(404).send(`No such course with id:${req.params.id} found!`);
-    res.send(course);
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course)
+    res.status(404).send(`No such course with id:${req.params.id} found!`);
+  res.send(course);
 });
-
-
-
-
-
-
 
 // app.listen(3000, () => console.log("Listening port 3000.."));
 // hard coded port numbers do not work in production environment.
