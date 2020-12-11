78fc34ff8e0c9e2afabe5ed0af835ea86fff198e
Vishal Ramane <vramane007@gmail.com>
2020-12-11 13:04:14 +0530

    changes to Modal window project

--- CodeKitchen/JavaScript/4_DOM_&_Events/p2_Modal_window/script.js
+++ CodeKitchen/JavaScript/4_DOM_&_Events/p2_Modal_window/script.js
@@ -6,7 +6,7 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 const openModal = function () {
-  console.log(`Button ${i} clicked.`);
+  //console.log(`model Button clicked.`);
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -22,3 +22,10 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  //console.log(`${e.key} key was pressed`);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
